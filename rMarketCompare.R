9cb54d27c6b38409c623e0ddf11b9ef141bff60d
waljock <waljock@gmail.com>
2018-01-20 20:59:05 -0800

    Rdata added

---
+++ rMarketCompare.R
@@ -0,0 +1,21 @@
+library(readxl)
+library(ggplot2)
+#library(plyr)
+
+d <- read_xlsx("C:/Users/Walter/Documents/GitHub/eve-marketcompare/combined_data.xlsx")
+d2 <-subset(d,(region == 'Catch'))
+count(d2$type_id)
+
+#hist(d2$type_id)
+
+hist(d2$average, breaks=10000)
+#44138d2
+
+catch <- subset(d, (type_id == 43894))
+
+catch
+pc <- ggplot(catch, aes(date, average)  )+
+  geom_jitter(aes(color = region))
+pc
+
+
