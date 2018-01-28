0758fbc79f070a1b03d70687a90896036623aca1
waljock <waljock@gmail.com>
2018-01-28 12:04:37 -0800

    final commit

--- rMarketCompare3.R
+++ rMarketCompare3.R
@@ -4,7 +4,12 @@ library(plyr)
 library(data.table)
 library(dplyr)
 
-d <- read_xlsx("C:/Users/Walter/Documents/GitHub/eve-marketcompare/combined_data.xlsx")
+
+d <- read.csv("C:/Users/Walter/Documents/GitHub/eve-marketcompare/combined_data.csv")
+types <- read_xls("C:/Users/Walter/Documents/GitHub/eve-marketcompare/InvTypes.xls")
+
+types <- types[c(1,3,5)]
+
 d$today <- Sys.Date()
 d$datediff <- as.numeric(difftime(d$today,  d$date))
 d <- subset(d,(datediff <= 30) & (average <=7500000) & (volume <=80))
@@ -13,32 +18,47 @@ c <- subset(d, region == 'Catch')
 j <- subset(d, region == 'Jita')
 
 
-dsorted <- c[order(c$region,c$type_id,c$date),]
-dsortedc <- j[order(j$region,j$type_id,j$date),]
+dsortedc <- c[order(c$region,c$type_id,c$date),]
+dsortedj <- j[order(j$region,j$type_id,j$date),]
 
 #dCatch <- subset(dsorted, d$region == 'Catch')
-dsorted$dolvol <- ((dsorted$average * dsorted$volume)/(dsorted$datediff))
-hist(dsorted$dolvol,breaks=100)
+dsortedc$dolvol <- ((dsortedc$average * dsortedc$volume)/(dsortedc$datediff))
+dsortedj$dolvol <- ((dsortedj$average * dsortedj$volume)/(dsortedj$datediff))
 
-d2 <- subset(dsorted, region == 'Catch')
-hist(d2$dolvol,breaks=100)
-hist(d2$volume,breaks=100)
+d2c <- subset(dsortedc, region == 'Catch')
+d2j <- subset(dsortedj, region == 'Jita')
+hist(d2c$dolvol,breaks=100)
+hist(d2j$volume,breaks=100)
 
-write.csv(d2,'C:/Users/Walter/Documents/GitHub/eve-marketcompare/catchHist.csv')
 
-d2$dd <- as.numeric(d2$datediff)
 
-d3 <- d2[c(1,6,7,12)]
+d2c$dd <- as.numeric(d2c$datediff)
+d2j$dd <- as.numeric(d2j$datediff)
 
-cars <- d3 %>%
+d3c <- d2c[c(1,6,7,12)]
+d3j <- d2j[c(1,6,7,12)]
+
+cat <- d3c %>%
   select (type_id, volume, average, dd) %>%
   group_by(type_id) %>%
   summarise(average=mean(average), dd=mean(dd), volume = mean(volume))
 
-plot(cars$average, cars$volume)
+jit <- d3j %>%
+  select (type_id, volume, average, dd) %>%
+  group_by(type_id) %>%
+  summarise(average=mean(average), dd=mean(dd), volume = mean(volume))
 
+plot(cat$average, cat$volume)
+plot(jit$average, jit$volume)
 
+s <- merge(cat, jit, by='type_id')
 
+s$profit <- s$average.x - s$average.y
+s$margin <- s$average.x/s$average.y
+
+fin <- merge(s, types, by.x='type_id', by.y='TYPEID')
+
+write.csv(fin,'C:/Users/Walter/Documents/GitHub/eve-marketcompare/finfin.csv')
 
 
 
